38220399a48b3c347618cd1348cd0e6182471df1
vivibarragango <viviana.barragango@gmail.com>
2023-12-09 00:28:56 -0500

    update newsletter 2

--- finalproject1/components/NewsLetter.tsx
+++ finalproject1/components/NewsLetter.tsx
@@ -5,9 +5,11 @@ import React, { useRef, useState } from 'react';
 
 export default function NewsLetter() {
   // 1. Create a reference to the input so we can fetch/clear it's value.
-  const inputEl = useRef(null);
+  const inputEl = useRef<HTMLInputElement>(null); // Specify the type as HTMLInputElement
   // 2. Hold a message in state to handle the response from our API.
+  const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  
 
   const subscribe = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -42,7 +44,7 @@ export default function NewsLetter() {
     <form onSubmit={subscribe}>
       <div className="flex w-full flex-wrap md:flex-nowrap flex-col gap-4" >
         <p className="mb-2 font-head">Subscribe to our Newsletter</p>
-        <Input ref={inputEl} type="email" label="Email" placeholder="Enter your email" />
+        <Input ref={inputEl} type="email" label="Email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)}/>
       </div>
 
       <Button type='submit' color="primary" variant="bordered">
