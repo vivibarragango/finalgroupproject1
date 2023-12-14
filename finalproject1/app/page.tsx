043a84c651b9418b4ce8af867142f423ffdd6837
vivibarragango <viviana.barragango@gmail.com>
2023-12-14 10:15:22 -0500

    created Reward component

--- finalproject1/app/page.tsx
+++ finalproject1/app/page.tsx
@@ -1,10 +1,14 @@
 import { HeroSection } from '@/components/HeroSection';
+import { Reward } from '@/components/Reward';
 import { Button } from '@nextui-org/react';
 import Image from "next/legacy/image";
 
 export default function Home() {
 	return (
+		<>
+			<HeroSection />
 
-		<HeroSection />
+			<Reward />
+		</>
 	);
 }
---
+++ finalproject1/components/Reward.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { Button } from '@nextui-org/react';
+import Image from "next/legacy/image";
+
+export const Reward = () => {
+	
+	return (
+		<section className="hero">
+					
+		</section>
+	);
+};
