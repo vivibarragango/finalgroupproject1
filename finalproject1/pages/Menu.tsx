d5b4b4a2eee75298812d85fd62a83785d86fcbc6
vivibarragango <viviana.barragango@gmail.com>
2023-12-18 23:06:48 -0500

    update menu file with more icons

--- finalproject1/pages/Menu.tsx
+++ finalproject1/pages/Menu.tsx
@@ -1,19 +1,30 @@
 // pages/Menu.tsx
 import React from 'react';
-import {Card, CardHeader, CardBody, CardFooter} from "@nextui-org/react";
+import { Card, CardBody } from "@nextui-org/react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { 
+  faMugHot, faCoffee, faCocktail, faLeaf, 
+  faWineBottle, faGlassWhiskey, faBlender, faIceCream
+} from '@fortawesome/free-solid-svg-icons';
 
 const MenuPage = () => {
   const menuItems = [
-    { name: 'Espresso', price: '$3.00' },
-    { name: 'Cappuccino', price: '$4.00' },
-    { name: 'Latte', price: '$4.50' },
-    { name: 'Mocha', price: '$4.75' },
-    { name: 'Americano', price: '$3.50' },
-    { name: 'Macchiato', price: '$3.75' },
-    { name: 'Flat White', price: '$4.25' },
-    { name: 'Iced Coffee', price: '$3.50' },
-    { name: 'Iced Latte', price: '$4.50' },
-    { name: 'Chai Latte', price: '$4.50' },
+    { name: 'Espresso', price: '$3.00', icon: faCoffee },
+    { name: 'Cappuccino', price: '$4.00', icon: faMugHot },
+    { name: 'Latte', price: '$4.50', icon: faMugHot },
+    { name: 'Mocha', price: '$4.75', icon: faGlassWhiskey },
+    { name: 'Americano', price: '$3.50', icon: faCoffee },
+    { name: 'Macchiato', price: '$3.75', icon: faCoffee },
+    { name: 'Flat White', price: '$4.25', icon: faCoffee },
+    { name: 'Iced Coffee', price: '$3.50', icon: faCocktail },
+    { name: 'Iced Latte', price: '$4.50', icon: faCocktail },
+    { name: 'Chai Latte', price: '$4.50', icon: faLeaf },
+    { name: 'Hot Chocolate', price: '$4.00', icon: faMugHot },
+    { name: 'Green Tea', price: '$3.00', icon: faLeaf },
+    { name: 'Black Tea', price: '$3.00', icon: faLeaf },
+    { name: 'Herbal Tea', price: '$3.00', icon: faLeaf },
+    { name: 'Lemonade', price: '$3.50', icon: faWineBottle },
+    { name: 'Iced Tea', price: '$3.50', icon: faIceCream },
     // Add more menu items here
   ];
 
@@ -23,9 +34,12 @@ const MenuPage = () => {
       <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
         {menuItems.map((item, index) => (
           <Card key={index} isHoverable isPressable style={{ margin: '10px', width: '300px' }}>
-            <CardBody style={{ padding: '20px' }}>
-              <h4 style={{ margin: '0' }}>{item.name}</h4>
-              <p style={{ margin: '5px 0 0 0' }}>{item.price}</p>
+            <CardBody style={{ padding: '20px', display: 'flex', alignItems: 'center', gap: '10px' }}>
+              <FontAwesomeIcon icon={item.icon} size="2x" />
+              <div>
+                <h4 style={{ margin: '0' }}>{item.name}</h4>
+                <p style={{ margin: '0' }}>{item.price}</p>
+              </div>
             </CardBody>
           </Card>
         ))}
