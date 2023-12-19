e38d9115e117aa2fd76c20079160370adc5d54ed
vivibarragango <viviana.barragango@gmail.com>
2023-12-19 09:47:01 -0500

    update link sboutUs from footer

--- finalproject1/components/UserTable.tsx
+++ finalproject1/components/UserTable.tsx
@@ -16,25 +16,25 @@ export default function UserTable() {
           <TableCell className="font-head">MENU</TableCell>
           <TableCell className="font-head">COMMUNITY</TableCell>
           <TableCell className="font-head">SOCIALS</TableCell>
-          <TableCell className="font-head"><Link href="/aboutUs" color="foreground">ABOUT US</Link></TableCell>
+          <TableCell className="font-head">ABOUT US</TableCell>
         </TableRow>
         <TableRow key="2">
           <TableCell className="font-column">Beverages</TableCell>
           <TableCell className="font-column">Events</TableCell>
           <TableCell className="font-column">Instagram</TableCell>
-          <TableCell className="font-column">Mission Statement</TableCell>
+          <TableCell className="font-column"><Link href="/aboutUs" color="foreground">Mission Statement</Link></TableCell>
         </TableRow>
         <TableRow key="3">
           <TableCell className="font-column">Bulk Brew</TableCell>
           <TableCell className="font-column">Active Partnerships</TableCell>
           <TableCell className="font-column">Facebook</TableCell>
-          <TableCell className="font-column">Corporate Values</TableCell>
+          <TableCell className="font-column"><Link href="/aboutUs" color="foreground">Corporate Values</Link></TableCell>
         </TableRow>
         <TableRow key="4">
           <TableCell className="font-column">Merchandise</TableCell>
           <TableCell className="font-column">Outreach Programs</TableCell>
           <TableCell className="font-column">YouTube</TableCell>
-          <TableCell className="font-column">Sourcing Commitments</TableCell>
+          <TableCell className="font-column"><Link href="/aboutUs" color="foreground">Sourcing Commitments</Link></TableCell>
         </TableRow>
         <TableRow key="5">
           <TableCell className="font-column">Gift Cards</TableCell>
